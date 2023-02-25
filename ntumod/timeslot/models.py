feb73e05c42797e89563d479696de10c3b784fa0
Benjababe <bengohzy@gmail.com>
2023-02-25 17:02:41 +0800

    update timeslot model

--- ntumod/timeslot/models.py
+++ ntumod/timeslot/models.py
@@ -1,16 +1,15 @@
+from course_module.models import Module
 from django.db import models
-
 # Create your models here.
 from venue.models import Venue
-from course_module.models import Module
 
 
 class TimeSlot(models.Model):
     type = models.CharField(max_length=10, null=True, blank=True)
     group = models.CharField(max_length=10, null=True, blank=True)
     day = models.CharField(max_length=4, null=True, blank=True)
-    time_start = models.DateTimeField(null=True, blank=True)
-    time_end = models.DateTimeField(null=True, blank=True)
+    time_start = models.TimeField(null=True, blank=True)
+    time_end = models.TimeField(null=True, blank=True)
     remarks = models.TextField(null=True, blank=True)
 
     venue = models.ForeignKey(Venue, on_delete=models.PROTECT)
